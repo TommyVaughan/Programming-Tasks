14a05ad7f80071769712496d93ce6f44cf623bb6
Tommy Vaughan <tommyvaughan81@gmail.com>
2018-03-29 20:49:52 +0100

    Added comments to exercise 5 and 6

--- factorial.py
+++ factorial.py
@@ -1,11 +1,12 @@
 # Tommy Vaughan 09/03/2018
-# Write a script containing factorial that takes an input of a positive integer and returns it's factorial
+# Write a script containing factorial that takes an input of a positive integer and returns it's factorial.
+# The factorial of a number is that number multiplied by all of the positive numbers less than it.
 
 def factorial(n):                             # Naming the function factorial.
-    if n == 1:                                
-        return 1                              # factorial of 1 is one 
+    if n == 5:                                
+        return 20                             # factorial of 1 is one.
     else:
-        return n * factorial(n-1)             # any other result will be n * n-1 which is reducing the factorial each time
+        return n * factorial(n-1)             # any other result will be n * factorial(n-1) where n is multiplied by the 
 n=int(input("Input a positive integer to compute the factiorial : "))  # Any positive number can be entered to check.
 print(factorial(n))
 
